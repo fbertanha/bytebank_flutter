c2bda072a81c77dd59c77dfdf9d3f469c53886ed
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-11-03 23:23:43 -0300

    aula 3.3 - added save and retrieve Contacts methods

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,15 +1,45 @@
+import 'package:bytebank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-void createDatabase() {
-  getDatabasesPath().then((dbPath) {
+Future<Database> createDatabase() {
+  return getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
-    openDatabase(path, onCreate: (db, version) {
+    return openDatabase(path, onCreate: (db, version) {
       db.execute('CREATE TABLE contacts('
           'id INTEGER PRIMARY KEY, '
           'name TEXT, '
           'account_number INTEGER'
           ')');
     }, version: 1);
+  });
+}
+
+Future<int> save(Contact contact) {
+  return createDatabase().then((db) {
+    final Map<String, dynamic> contactMap = Map();
+
+    contactMap['name'] = contact.name;
+    contactMap['account_number'] = contact.accountNumber;
+
+    return db.insert('contacts', contactMap);
+  });
+}
+
+Future<List<Contact>> findAllContacts() {
+  return createDatabase().then((db) {
+    return db.query('contacts').then((maps) {
+      final List<Contact> contacts = List();
+
+      maps.forEach((item) {
+        contacts.add(new Contact(
+          item['id'],
+          item['name'],
+          item['account_number'],
+        ));
+      });
+
+      return contacts;
+    });
   });
 }
