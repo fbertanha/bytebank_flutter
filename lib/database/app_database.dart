ccc0430b676d3ae4c47e52caea6a8c927affedfa
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-11-05 12:36:20 -0300

    aula 5.2 - refactoring to async/await

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,44 +2,44 @@ import 'package:bytebank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(path, onCreate: (db, version) {
+Future<Database> getDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
       db.execute('CREATE TABLE contacts('
           'id INTEGER PRIMARY KEY, '
           'name TEXT, '
           'account_number INTEGER'
           ')');
-    }, version: 1, onDowngrade: onDatabaseDowngradeDelete);
-  });
+    },
+    version: 1,
+    onDowngrade: onDatabaseDowngradeDelete,
+  );
 }
 
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = Map();
+Future<int> save(Contact contact) async {
+  final Database db = await getDatabase();
+  final Map<String, dynamic> contactMap = Map();
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
 
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-
-    return db.insert('contacts', contactMap);
-  });
+  return db.insert('contacts', contactMap);
 }
 
-Future<List<Contact>> findAllContacts() {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = List();
+Future<List<Contact>> findAllContacts() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  final List<Contact> contacts = List();
 
-      maps.forEach((item) {
-        contacts.add(new Contact(
-          item['id'],
-          item['name'],
-          item['account_number'],
-        ));
-      });
-
-      return contacts;
-    });
+  result.forEach((item) {
+    contacts.add(new Contact(
+      item['id'],
+      item['name'],
+      item['account_number'],
+    ));
   });
+
+  return contacts;
 }
