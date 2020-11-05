751a0f8f180dd5cc4120bf299f5daeb7fc52f55d
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-11-04 23:30:16 -0300

    aula 4.1 - added contacts list

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -3,27 +3,23 @@ import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
+    contacts.add(Contact(0, 'Alex', 1000));
+    contacts.add(Contact(1, 'Felipe',2000));
     return Scaffold(
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Nome',
-                style: TextStyle(fontSize: 24),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -37,4 +33,29 @@ class ContactsList extends StatelessWidget {
       ),
     );
   }
+}
+
+class _ContactItem extends StatelessWidget {
+
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+
 }
