e4644c4d1fa43e60ed3d4f86bdaa73cb219658e6
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-11-06 18:29:26 -0300

    aula 3.1 - parsing json to object

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
@@ -15,11 +19,30 @@ class LoggingInterceptor implements InterceptorContract {
   }
 }
 
-void findAll() async {
+Future<List<Transaction>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(interceptors: [
     LoggingInterceptor(),
   ]);
 
   final Response response =
       await client.get('http://192.168.0.100:8080/transactions');
+
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = List();
+
+  decodedJson.forEach((item) {
+    final Transaction transaction = Transaction(
+        item['value'],
+        Contact(
+          0,
+          item['contact']['name'],
+          item['contact']['accountNumber'],
+        ));
+
+    transactions.add(transaction);
+  });
+  // print(decodedJson);
+  // print(transactions);
+
+  return transactions;
 }
