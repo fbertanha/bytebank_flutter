629ad2a031c8475222a8d73026b75eedae383216
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2020-11-06 17:01:05 -0300

    aula 2.3 - added http_interceptor package

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,25 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data);
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get('http://192.168.0.100:8080/transactions');
-  print(response.body);
+  final Client client = HttpClientWithInterceptor.build(interceptors: [
+    LoggingInterceptor(),
+  ]);
+
+  final Response response =
+      await client.get('http://192.168.0.100:8080/transactions');
 }
